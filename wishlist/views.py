6a60d7eb0a4ca7fea34fbea59a524987563289da
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2022-09-22 12:27:56 +0700

    Merestriksi Akses Halaman Wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -11,7 +11,10 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+@login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
