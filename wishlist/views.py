c8455c743fee0345bda3925c824992b95ae1c323
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2022-09-22 12:50:50 +0700

    Menambahkan Cookies

--- wishlist/views.py
+++ wishlist/views.py
@@ -13,13 +13,18 @@ from django.contrib.auth import logout
 
 from django.contrib.auth.decorators import login_required
 
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 # Create your views here.
 @login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
         'list_barang': data_barang_wishlist,
-        'nama': 'Lyzander Marciano Andrylie'
+        'nama': 'Lyzander Marciano Andrylie',
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "wishlist.html", context)
@@ -63,8 +68,10 @@ def login_user(request):
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
-            return redirect('wishlist:show_wishlist')
+            login(request, user) # melakukan login terlebih dahulu
+            response = HttpResponseRedirect(reverse("wishlist:show_wishlist")) # membuat response
+            response.set_cookie('last_login', str(datetime.datetime.now())) # membuat cookie last_login dan menambahkannya ke dalam response
+            return response
         else:
             messages.info(request, 'Username atau Password salah!')
     context = {}
@@ -72,4 +79,6 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    return redirect('wishlist:login')
+    response = HttpResponseRedirect(reverse('wishlist:login'))
+    response.delete_cookie('last_login')
+    return response
